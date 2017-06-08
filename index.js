2408a7483356eadad2b38acb4d444de108868edf
Emmanuel Manyike <emmanuel.manyike@manalto.com>
2017-06-08 22:58:46 +0200

    Grouping the tests for per function

--- index.js
+++ index.js
@@ -38,24 +38,24 @@ function weekNumberForGivenDate (fullYear, fullMonth, dateOfTheMonth) {
 		return 'full year should be four digits e.g 2017'
 	}
 
-	if (parseInt(fullMonth) > 12 || parseInt(fullMonth) < 1) {
+	if (parseInt(fullMonth, 10) > 12 || parseInt(fullMonth, 10) < 1) {
 		return 'Full month should be between 1 and 12'
 	}
 
-	if (fullMonth.length > 1 && parseInt(fullMonth) < 10) {
+	if (fullMonth.length > 1 && parseInt(fullMonth, 10) < 10) {
 		return 'Month should be between 1 and 12 i.e >=1 and <=12 and not 01 or 09';
 	}
 
-	if (dateOfTheMonth.length > 1 && parseInt(dateOfTheMonth) < 10) {
+	if (dateOfTheMonth.length > 1 && parseInt(dateOfTheMonth, 10) < 10) {
 		return 'dateOfTheMonth should be between 1 and 31 i.e >=1 and <=31 and not 01 or 09';
 	}
 
-	if (parseInt(dateOfTheMonth) > 31) {
+	if (parseInt(dateOfTheMonth, 10) > 31) {
 		return 'dateOfTheMonth should be between 1 and 31 i.e >=1 and <=31';
 	}
 
-	let paddedDayOfTheMonth = parseInt(dateOfTheMonth) > 9 ? dateOfTheMonth : '0' + dateOfTheMonth;
-	let paddedMonth = parseInt(fullMonth) > 9 ? fullMonth : '0' + fullMonth;
+	let paddedDayOfTheMonth = parseInt(dateOfTheMonth, 10) > 9 ? dateOfTheMonth : '0' + dateOfTheMonth;
+	let paddedMonth = parseInt(fullMonth, 10) > 9 ? fullMonth : '0' + fullMonth;
 
 	if (!moment(`${fullYear}-${paddedMonth}-${paddedDayOfTheMonth}`).isValid()) {
 		return 'supplied date is not valid';
